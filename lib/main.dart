57188a5952beb0080be535faafefe4c0a229fae4
nurbek <amanbekov.nurbek@gmail.com>
2023-04-13 19:24:23 +0600

    Init map

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'config/providers.dart';
 import 'config/routes.dart';
 
 void main() {
-  runApp(MultiProvider(
-    providers: appProviders,
-    child: const ShtushnyiApp(),
-  ));
+  runApp(const ShtushnyiApp());
 }
 
 class ShtushnyiApp extends StatelessWidget {
-  const ShtushnyiApp({Key? key}) : super(key: key);
+  const ShtushnyiApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final scaffoldKey = GlobalKey<ScaffoldMessengerState>();
-
     return MaterialApp(
-      scaffoldMessengerKey: scaffoldKey,
       title: 'Shtushnyi App',
       routes: AppRouter().routes,
       initialRoute: AppRouter.home,
